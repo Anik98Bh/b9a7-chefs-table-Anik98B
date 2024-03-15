173b6a3cab60897b8344976b09c2fcdff44d7e81
Anik98B <anikbhowmik0188@gmail.com>
2024-03-16 00:25:40 +0600

    added nav section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,54 @@
-
+// import background from '../../assets/Banner.jpg'
 
 const Header = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold">
-                Hello world!
-            </h1>
+            {/* navber */}
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-3xl font-bold">Recipe Calories</a>
+                </div>
+                <div className="navbar-center mr-44 flex gap-20">
+                    <a className="text-[#150B2BB3]">Home</a>
+                    <a className="text-[#150B2BB3]">Recipes</a>
+                    <a className="text-[#150B2BB3]">About</a>
+                    <a className="text-[#150B2BB3]">Search</a>
+                </div>
+                <div className="flex-none gap-2">
+                    <label className="input input-bordered rounded-full bg-[#150B2B0D] flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                        <input type="text" className="grow" placeholder="Search" />
+                    </label>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            {/*  */}
+            <div className="hero min-h-screen" style={{ backgroundImage: <img></img> }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
